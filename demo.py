8c0c25e8841e314e1ebd0f3463cab7634aede7d4
lngy <lebinhnguyen2004@gmail.com>
2024-12-21 15:08:06 +0700

    update

--- demo.py
+++ demo.py
@@ -90,7 +90,7 @@ def main(args):
     # 获取视频的宽度和高度
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
+    os.makedirs('output', exist_ok=True)
     video_out = cv2.VideoWriter('output/output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))  
 
     detector = Detector()
